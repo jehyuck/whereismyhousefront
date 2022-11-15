9edb768611e2b3f45cb93198f6dd22224dcb259f
heedd <hylee011@naver.com>
2022-11-15 14:29:45 +0900

    확장자

--- src/router/index.js
+++ src/router/index.js
@@ -1,100 +1,66 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "@/views/HomeView";
-import LoginView from "@/views/LoginView";
-import BookView from "@/views/BookView";
-import BookListView from "@/components/book/BookListView";
-import BookRegistView from "@/components/book/BookRegistView";
-import BookDetailView from "@/components/book/BookDetailView";
-import BookModifyView from "@/components/book/BookModifyView";
-import QNAView from "@/views/QNAView";
-import QNAListView from "@/components/qna/QNAListView";
-import QNADetailView from "@/components/qna/QNADetailView";
-import QNAModifyView from "@/components/qna/QNAModifyView";
-import QNARegistView from "@/components/qna/QNARegistView";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '@/views/HomeView';
+import LoginView from '@/views/LoginView';
+import QNAView from '@/views/QNAView';
+import QNAListView from '@/components/qna/QNAListView';
+import QNADetailView from '@/components/qna/QNADetailView';
+import QNAModifyView from '@/components/qna/QNAModifyView';
+import QNARegistView from '@/components/qna/QNARegistView';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: HomeView,
   },
   {
-    path: "/login",
-    name: "login",
+    path: '/login',
+    name: 'login',
     component: LoginView,
   },
   {
-    path: "/book",
-    name: "BookView",
-    component: BookView,
-    redirect: "/book/booklist",
+    path: '/qna',
+    name: 'qna',
+    component: QNAView,
+    redirect: 'qna/qnalist',
     children: [
       {
-        path: "booklist",
-        name: "BookListView",
-        component: BookListView,
+        path: 'qnalist',
+        name: 'QNAListView',
+        component: QNAListView,
       },
       {
-        path: "bookregist",
-        name: "BookRegistView",
-        component: BookRegistView,
+        path: 'qnadetail',
+        name: 'QNADetailView',
+        component: QNADetailView,
       },
       {
-        path: "bookdetail",
-        name: "BookDetailView",
-        component: BookDetailView,
+        path: 'qnamodify',
+        name: 'QNAModifyView',
+        component: QNAModifyView,
       },
       {
-        path: "bookmodify",
-        name: "BookModifyView",
-        component: BookModifyView,
+        path: 'qnaregist',
+        name: 'QNARegistView',
+        component: QNARegistView,
       },
     ],
   },
   {
-    path: "/qna",
-    name: "qna",
-    component: QNAView,
-    redirect: "qna/qnalist",
-    children: [
-      {
-        path: "qnalist",
-        name: "QNAListView",
-        component: QNAListView,
-      },
-      {
-          path: "qnadetail",
-          name: "QNADetailView",
-          component: QNADetailView,
-        },
-        {
-        path: "qnamodify",
-        name: "QNAModifyView",
-        component: QNAModifyView,
-      },
-      {
-        path: "qnaregist",
-        name: "QNARegistView",
-        component: QNARegistView,
-      },
-    ]
-  },
-  {
-    path: "/about",
-    name: "about",
+    path: '/about',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
