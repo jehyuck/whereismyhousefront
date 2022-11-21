805453a62844dcebdcb29e3f6987c50077328a94
heedd <hylee011@naver.com>
2022-11-21 16:14:25 +0900

    fix: router.push error handling

--- src/router/index.js
+++ src/router/index.js
@@ -190,4 +190,12 @@ const router = new VueRouter({
   routes,
 });
 
+//this.$router.push 에러핸들링
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => {
+    if (err.name !== 'NavigationDuplicated') throw err;
+  });
+};
+
 export default router;
